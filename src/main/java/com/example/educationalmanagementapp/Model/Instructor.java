11cbd2655cf618c7c144c10a173fda001a86acee
AJAO Hazzan <58217780+HazzanAj@users.noreply.github.com>
2021-12-09 04:13:36 +0200

    Update Instructor.java

--- src/main/java/com/example/educationalmanagementapp/Model/Instructor.java
+++ src/main/java/com/example/educationalmanagementapp/Model/Instructor.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Instructor  implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(nullable = false, updatable = false)// we don't need to pass this it will be generated automatically.
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String instructorName;
     private String instructorCourse;
